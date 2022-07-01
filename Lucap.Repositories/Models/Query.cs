1d7371f40663dfec8706940fd5b0e472b135ac3c
Malka Bruk <malka2039@gmail.com>
2022-07-01 12:54:40 +0300

    EF

---
+++ Lucap.Repositories/Models/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lucap.Repositories.Models
+{
+    public partial class Query
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? ConnectionId { get; set; }
+        public string? Query1 { get; set; }
+
+        public virtual Connection? Connection { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
--- Lucap.Web/Program.cs
+++ Lucap.Web/Program.cs
@@ -1,6 +1,18 @@
+using Lucap.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+if(builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets("c7dd08bf-554c-4650-a39e-a7a86d031c1a");
+}
+
 // Add services to the container.
+//builder.Configuration.AddUserSecrets()
+var cs = builder.Configuration["LucapDBConnectionString"];
+Console.WriteLine(cs);
+builder.Services.AddDbContext<LucapDBContext>(options => options.UseNpgsql(cs));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
